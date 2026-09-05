31e7dd1003129a48a48723922d238670dc7d7e2e
Walmyr <wlsf82@gmail.com>
2026-09-05 21:57:04 +0200

    Write the operators JS file

---
+++ 03-operators.js
@@ -0,0 +1,16 @@
+let count = 0
+count += 1
+count += 1
+console.log(count)
+
+console.log('2' + 3)
+console.log(2 + 3)
+
+const statusFromPage = '200'
+console.log(statusFromPage == 200)
+console.log(statusFromPage === 200)
+console.log(Number(statusFromPage) === 200)
+
+const statusCode = 200
+const customerName = 'Acme'
+console.log(statusCode === 200 && customerName !== '')
