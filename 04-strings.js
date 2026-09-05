e9564a17a3883ceecd3cf51ea36f3cfdf7453e83
Walmyr <wlsf82@gmail.com>
2026-09-05 22:19:07 +0200

    Write the strings JS file

---
+++ 04-strings.js
@@ -0,0 +1,15 @@
+const firstName = 'Walmyr'
+const lastName = 'Filho'
+
+console.log(firstName + ' ' + lastName)
+console.log(`${firstName} ${lastName}`)
+
+const name = 'Acme'
+const size = 'Large'
+const employees = 240
+console.log(`O cliente ${name} é ${size} com ${employees} funcionários`)
+
+const customerId = 42
+console.log(`/customers/${customerId}`)
+
+console.log(`walmyr+${Date.now()}@example.com`)
